a3560c6afd5b8fb95526082a3c2f907ac2fd1b15
TranML <the44secrets@gmail.com>
2025-06-29 22:22:43 +0700

    MediaPlayerScreen: listen to timeUpdate to catch currentTime

--- src/app/media-player.tsx
+++ src/app/media-player.tsx
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import { Stack } from "expo-router";
 import { useVideoPlayer, VideoView } from "expo-video";
 import MediaPlayer from "../components/MediaPlayer";
+import { useEvent, useEventListener } from "expo";
+import { useEffect } from "react";
 
 const videoSource =
   "https://res.cloudinary.com/dqssqzt3y/video/upload/v1750664615/unit-4--part-4-of-4_aowxmp.mp4";
@@ -9,9 +11,13 @@ const videoSource =
 export default function MediaPlayerScreen() {
   const mediaPlayer = useVideoPlayer(videoSource, (player) => {
     player.showNowPlayingNotification = true;
-    player.play();
+    player.timeUpdateEventInterval = 0.5;
+    // player.play();
   });
 
+  useEventListener(mediaPlayer, "timeUpdate", (event) => {
+    console.log("Current time: ", event.currentTime);
+  });
 
   return (
     <View>
