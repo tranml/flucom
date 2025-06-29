6f8a85d4fe4c1d6f8e19be05776789e6df2e443d
TranML <the44secrets@gmail.com>
2025-06-29 13:56:09 +0700

    create RootLayout with Stack

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Stack } from "expo-router";
 
 export default function App() {
   return (
     <View>
       <Text>Welcome to Flucom</Text>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </View>
   );
 }
