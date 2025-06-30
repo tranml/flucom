680de4457f9b45911a36425191cdded81d0633f2
TranML <the44secrets@gmail.com>
2025-06-30 09:19:16 +0700

    MediaPlayer: toggle play/pause

--- src/app/media-player.tsx
+++ src/app/media-player.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 import { Stack } from "expo-router";
 import { useVideoPlayer, VideoView } from "expo-video";
 import MediaPlayer from "../components/MediaPlayer";
-import { useEventListener } from "expo";
-import { media} from "../lib/media-data";
+import { useEvent, useEventListener } from "expo";
+import { media } from "../lib/media-data";
 
 const mediaSource = media[1].url;
 
@@ -14,13 +14,19 @@ export default function MediaPlayerScreen() {
     // player.play();
   });
 
+  const { isPlaying } = useEvent(mediaPlayer, "playingChange", {
+    isPlaying: mediaPlayer.playing,
+  });
+
   useEventListener(mediaPlayer, "timeUpdate", (event) => {
     console.log("Current time: ", event.currentTime);
   });
 
+  console.log("isPlaying", isPlaying);
+
   return (
     <View>
-      <MediaPlayer mediaPlayer={mediaPlayer} />
+      <MediaPlayer mediaPlayer={mediaPlayer} isPlaying={isPlaying} />
       <Stack.Screen options={{ title: "Media Player" }} />
     </View>
   );
--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -6,12 +6,17 @@ import {
 } from "react-native";
 import { VideoPlayer, VideoView } from "expo-video";
 import { useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 type MediaPlayerProps = {
   mediaPlayer: VideoPlayer;
+  isPlaying: boolean;
 };
 
-export default function MediaPlayer({ mediaPlayer }: MediaPlayerProps) {
+export default function MediaPlayer({
+  mediaPlayer,
+  isPlaying,
+}: MediaPlayerProps) {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
@@ -24,21 +29,39 @@ export default function MediaPlayer({ mediaPlayer }: MediaPlayerProps) {
           <ActivityIndicator size="large" color="black" />
         </ImageBackground>
       )}
-      <VideoView
-        player={mediaPlayer}
-        style={{ width: "100%", aspectRatio: 16 / 9 }}
-        onFirstFrameRender={() => setIsLoading(false)}
-      />
+      <View style={styles.videoContainer}>
+        <VideoView
+          player={mediaPlayer}
+          style={{ width: "100%", aspectRatio: 16 / 9 }}
+          onFirstFrameRender={() => setIsLoading(false)}
+        />
+        <Ionicons
+          onPress={() => {
+            if (isPlaying) {
+              mediaPlayer.pause();
+            } else {
+              mediaPlayer.play();
+            }
+          }}
+          name={isPlaying ? "pause-circle" : "play-circle"}
+          size={72}
+          color="black"
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
   },
   imageBackground: {
     justifyContent: "center",
     opacity: 0.6,
+  },
+  videoContainer: {
+    gap: 10,
+    alignItems: "center",
   },
 });
