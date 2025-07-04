b3ce33648924e3355b3e92f048871ff43d0fb02b
TranML <the44secrets@gmail.com>
2025-07-04 11:37:37 +0700

    create sub6

---
+++ src/lib/sub6.ts
@@ -0,0 +1,472 @@
+import { SubtitleEntry } from "../types";
+
+export const sub6: SubtitleEntry[] = [
+  {
+    endTime: 1709,
+    id: 1,
+    phonetic: "həˈloʊ əˈɡɛn",
+    speaker: "Paul Gruber:",
+    startTime: 300,
+    text: "Hello again,",
+    translation: "Xin chào lần nữa,",
+  },
+  {
+    endTime: 8695,
+    id: 2,
+    phonetic:
+      "aɪ hoʊp ju ɡeɪv jɚˈsɛlf ˈplɛnti əv taɪm tə ˈpræktɪs ðoʊz ˌtiːˈeɪtʃ ˈsɛntənsəz ˈsloʊli",
+    speaker: "none",
+    startTime: 1709,
+    text: "I hope you gave yourself plenty of time to practice those TH sentences slowly.",
+    translation:
+      "Tôi hy vọng bạn đã cho mình đủ thời gian để luyện những câu có âm TH một cách chậm rãi.",
+  },
+  {
+    endTime: 13909,
+    id: 3,
+    phonetic:
+      "naʊ ɪn ðɪs ˈvɪdiˌoʊ wɪɚ ˈɡoʊɪŋ tə ɡoʊ θru ðoʊz seɪm ˈsɛntənsəz əˈɡɛn",
+    speaker: "none",
+    startTime: 8695,
+    text: "Now in this video, we're going to go through those same sentences again,",
+    translation:
+      "Bây giờ trong video này, chúng ta sẽ luyện lại những câu giống như vậy,",
+  },
+  {
+    endTime: 17399,
+    id: 4,
+    phonetic: "ˈoʊnli ðɪs taɪm mʌtʃ ˈfæstɚ",
+    speaker: "none",
+    startTime: 13909,
+    text: "only this time much faster.",
+    translation: "chỉ là lần này nhanh hơn nhiều.",
+  },
+  {
+    endTime: 24499,
+    id: 5,
+    phonetic:
+      "hɪɚ wi ɡoʊ ənd meɪk ʃʊr ju kən hɪɚ ðæt ɛɚ ˈfrɪkʃən ˈkʌmɪŋ aʊt ɑn itʃ ˌtiːˈeɪtʃ saʊnd",
+    speaker: "none",
+    startTime: 17399,
+    text: "Here we go, and make sure you can hear that air friction coming out on each TH sound.",
+    translation:
+      "Bắt đầu nào, và hãy chắc chắn rằng bạn có thể nghe được âm gió phát ra ở mỗi âm TH.",
+  },
+  {
+    endTime: 26647,
+    id: 6,
+    phonetic: "rɪˈpiːt ˈæftɚ mi",
+    speaker: "none",
+    startTime: 24529,
+    text: "Repeat after me.",
+    translation: "Lặp lại theo tôi nhé.",
+  },
+  {
+    endTime: 40058,
+    id: 7,
+    phonetic: "ˈθɛlmə sɛd θæŋk ju wɛn ʃi eɪt ðə θɪk ˈbɑrbɪˌkju ˈʧɪkən θaɪz",
+    speaker: "none",
+    startTime: 26647,
+    text: "Thelma said thank you when she ate the thick barbecue chicken thighs.",
+    translation: "Thelma đã nói cảm ơn khi cô ấy ăn đùi gà nướng dày.",
+  },
+  {
+    endTime: 48651,
+    id: 8,
+    phonetic: "ˈθɛlmə sɛd θæŋk ju wɛn ʃi eɪt ðə θɪk ˈbɑrbɪˌkju ˈʧɪkən θaɪz",
+    speaker: "none",
+    startTime: 40299,
+    text: "Thelma said thank you when she ate the thick barbecue chicken thighs.",
+    translation: "Thelma đã nói cảm ơn khi cô ấy ăn đùi gà nướng dày.",
+  },
+  {
+    endTime: 61080,
+    id: 9,
+    phonetic: "ðə ˈθɔtfəl θɪŋ aɪ θɔt əˈbaʊt wəz ðɛɚ ˈθæŋksˌɡɪvɪŋ θimd ˈpɑrti",
+    speaker: "none",
+    startTime: 50668,
+    text: "The thoughtful thing I thought about was their Thanksgiving-themed party.",
+    translation:
+      "Điều chu đáo mà tôi đã nghĩ đến là bữa tiệc theo chủ đề Lễ Tạ Ơn của họ.",
+  },
+  {
+    endTime: 75449,
+    id: 10,
+    phonetic: "ðə ˈθɔtfəl θɪŋ aɪ θɔt əˈbaʊt wəz ðɛɚ ˈθæŋksˌɡɪvɪŋ θimd ˈpɑrti",
+    speaker: "none",
+    startTime: 67735,
+    text: "The thoughtful thing I thought about was their Thanksgiving-themed party.",
+    translation:
+      "Điều chu đáo mà tôi đã nghĩ đến là bữa tiệc theo chủ đề Lễ Tạ Ơn của họ.",
+  },
+  {
+    endTime: 93571,
+    id: 11,
+    phonetic:
+      "ɑn ˈθɝzˌdeɪ ðə ˈθɛrəpɪst θɔt ðæt ðə θɪk θɚˈmɑmətɚz wɚ ˈstoʊlən baɪ θivz",
+    speaker: "none",
+    startTime: 79614,
+    text: "On Thursday, the therapist thought that the thick thermometers were stolen by thieves.",
+    translation:
+      "Vào thứ Năm, nhà trị liệu nghĩ rằng những cái nhiệt kế dày đã bị những tên trộm lấy cắp.",
+  },
+  {
+    endTime: 108384,
+    id: 12,
+    phonetic:
+      "ɑn ˈθɝzˌdeɪ ðə ˈθɛrəpɪst θɔt ðæt ðə θɪk θɚˈmɑmətɚz wɚ ˈstoʊlən baɪ θivz",
+    speaker: "none",
+    startTime: 99548,
+    text: "On Thursday, the therapist thought that the thick thermometers were stolen by thieves.",
+    translation:
+      "Vào thứ Năm, nhà trị liệu nghĩ rằng những cái nhiệt kế dày đã bị những tên trộm lấy cắp.",
+  },
+  {
+    endTime: 129168,
+    id: 13,
+    phonetic:
+      "ˈθiədɔɚ hu wəz ə ˈvɛri θɪn mæn peɪd ˈoʊnli ˈθɝti-dɑlər fɚ hɪz ˈθaɪˌrɔɪd ɪɡˈzæm",
+    speaker: "none",
+    startTime: 113815,
+    text: "Theodore, who was a very thin man, paid only $30 for his thyroid exam.",
+    translation:
+      "Theodore, một người đàn ông rất gầy, chỉ trả 30 đô la cho buổi khám tuyến giáp của mình.",
+  },
+  {
+    endTime: 146249,
+    id: 14,
+    phonetic:
+      "ˈθiədɔɚ hu wəz ə ˈvɛri θɪn mæn peɪd ˈoʊnli ˈθɝti-dɑlər fɚ hɪz ˈθaɪˌrɔɪd ɪɡˈzæm",
+    speaker: "none",
+    startTime: 134509,
+    text: "Theodore, who was a very thin man, paid only $30 for his thyroid exam.",
+    translation:
+      "Theodore, một người đàn ông rất gầy, chỉ trả 30 đô la cho buổi khám tuyến giáp của mình.",
+  },
+  {
+    endTime: 160512,
+    id: 15,
+    phonetic: "ˈθænoʊz ˈθisɪs wəz ˈθɝoʊ ənd kənˈteɪnd faɪv-ˈθaʊzənd wɝdz",
+    speaker: "none",
+    startTime: 148099,
+    text: "Thano's thesis was thorough and contained 5,000 words.",
+    translation: "Luận văn của Thano rất kỹ lưỡng và có 5.000 từ.",
+  },
+  {
+    endTime: 174281,
+    id: 16,
+    phonetic: "ˈθænoʊz ˈθisɪs wəz ˈθɝoʊ ənd kənˈteɪnd faɪv-ˈθaʊzənd wɝdz",
+    speaker: "none",
+    startTime: 164885,
+    text: "Thano's thesis was thorough and contained 5,000 words.",
+    translation: "Luận văn của Thano rất kỹ lưỡng và có 5.000 từ.",
+  },
+  {
+    endTime: 186713,
+    id: 17,
+    phonetic: "ˈmɪstɚ ˈθætʃɚ wəz ˈvɛri ˈθɔtfəl tə peɪ fɚ ðiz ˈθɛrəpi ˈsɛʃənz",
+    speaker: "none",
+    startTime: 174385,
+    text: "Mr. Thatcher was very thoughtful to pay for these therapy sessions.",
+    translation:
+      "Ông Thatcher đã rất chu đáo khi trả tiền cho các buổi trị liệu này.",
+  },
+  {
+    endTime: 198201,
+    id: 18,
+    phonetic: "ˈmɪstɚ ˈθætʃɚ wəz ˈvɛri ˈθɔtfəl tə peɪ fɚ ðiz ˈθɛrəpi ˈsɛʃənz",
+    speaker: "none",
+    startTime: 190756,
+    text: "Mr. Thatcher was very thoughtful to pay for these therapy sessions.",
+    translation:
+      "Ông Thatcher đã rất chu đáo khi trả tiền cho các buổi trị liệu này.",
+  },
+  {
+    endTime: 215393,
+    id: 19,
+    phonetic: "ðoʊz ˈmjuzɪkəl θimz meɪk ðə ˈbrʌðɚz fil laɪk ðeɪ ɑr ɪn ə ˈθiətɚ",
+    speaker: "none",
+    startTime: 200414,
+    text: "Those musical themes make the brothers feel like they are in a theater.",
+    translation:
+      "Những giai điệu âm nhạc đó khiến các anh em cảm thấy như đang ở trong rạp hát.",
+  },
+  {
+    endTime: 226682,
+    id: 20,
+    phonetic: "ðoʊz ˈmjuzɪkəl θimz meɪk ðə ˈbrʌðɚz fil laɪk ðeɪ ɑr ɪn ə ˈθiətɚ",
+    speaker: "none",
+    startTime: 219270,
+    text: "Those musical themes make the brothers feel like they are in a theater.",
+    translation:
+      "Những giai điệu âm nhạc đó khiến các anh em cảm thấy như đang ở trong rạp hát.",
+  },
+  {
+    endTime: 233915,
+    id: 21,
+    phonetic: "ˈɔsəm ju ɑr ˈduɪŋ ɡreɪt",
+    speaker: "none",
+    startTime: 230416,
+    text: "Awesome, you are doing great.",
+    translation: "Tuyệt vời, bạn đang làm rất tốt.",
+  },
+  {
+    endTime: 240603,
+    id: 22,
+    phonetic:
+      "oʊˈkeɪ naʊ aɪm ˈɡənə ɡɪv ju ə ɡreɪt ənd ˈʧælɪnʤɪŋ tiːˈeɪʧ tʌŋ ˈtwɪstɚ",
+    speaker: "none",
+    startTime: 233915,
+    text: "Okay, now I'm gonna give you a great and challenging TH tongue twister.",
+    translation:
+      "Được rồi, bây giờ tôi sẽ đưa cho bạn một câu xoắn lưỡi TH tuyệt vời và đầy thử thách.",
+  },
+  {
+    endTime: 248521,
+    id: 23,
+    phonetic:
+      "ju meɪ wɪʃ tə ˈpræktɪs ðɪs ˈmɛni taɪmz tə ɡɛt just tə ðə ˈfilɪŋ ʌv jʊɚ tʌŋ ˈkʌmɪŋ aʊt fɚ itʃ ʌv ðiz tiːˈeɪʧ saʊndz",
+    speaker: "none",
+    startTime: 240603,
+    text: "You may wish to practice this many times to get used to the feeling of your tongue coming out for each of these TH sounds.",
+    translation:
+      "Bạn có thể muốn luyện tập điều này nhiều lần để làm quen với cảm giác lưỡi của bạn đưa ra cho từng âm TH này.",
+  },
+  {
+    endTime: 250918,
+    id: 24,
+    phonetic: "hɪr wi ɡoʊ rɪˈpit ˈæftɚ mi",
+    speaker: "none",
+    startTime: 248521,
+    text: "Here we go, repeat after me.",
+    translation: "Bắt đầu nhé, lặp lại theo tôi.",
+  },
+  {
+    endTime: 253807,
+    id: 25,
+    phonetic: "aɪ θɔt ʌv ə θɔt",
+    speaker: "none",
+    startTime: 250918,
+    text: "I thought of a thought,",
+    translation: "Tôi đã nghĩ về một ý nghĩ,",
+  },
+  {
+    endTime: 258367,
+    id: 26,
+    phonetic: "bət ðə θɔt aɪ θɔt ˈwəzənt ðə θɔt aɪ θɔt aɪ θɔt",
+    speaker: "none",
+    startTime: 253807,
+    text: "but the thought I thought wasn't the thought I thought I thought.",
+    translation:
+      "nhưng ý nghĩ mà tôi nghĩ không phải là ý nghĩ mà tôi nghĩ tôi đã nghĩ.",
+  },
+  {
+    endTime: 262684,
+    id: 27,
+    phonetic: "ɪf ðə θɔt aɪ θɔt aɪ θɔt hæd bɪn ðə θɔt aɪ θɔt",
+    speaker: "none",
+    startTime: 258367,
+    text: "If the thought I thought I thought had been the thought I thought,",
+    translation: "Nếu ý nghĩ mà tôi nghĩ tôi đã nghĩ là ý nghĩ mà tôi đã nghĩ,",
+  },
+  {
+    endTime: 269018,
+    id: 28,
+    phonetic: "aɪ ˈwʊdənt həv θɔt soʊ mʌʧ",
+    speaker: "none",
+    startTime: 262684,
+    text: "I wouldn't have thought so much.",
+    translation: "thì tôi đã không nghĩ nhiều đến thế.",
+  },
+  {
+    endTime: 285372,
+    id: 29,
+    phonetic:
+      "oʊˈkeɪ naʊ lɛts du ɪt mʌʧ ˈfæstɚ hɪr wi ɡoʊ æz fæst æz wi ˈpɑsəbli kæn",
+    speaker: "none",
+    startTime: 278993,
+    text: "Okay, now let's do it much faster. Here we go, as fast as we possibly can.",
+    translation:
+      "Được rồi, bây giờ hãy làm điều đó nhanh hơn nhiều. Bắt đầu nào, nhanh nhất có thể.",
+  },
+  {
+    endTime: 287799,
+    id: 30,
+    phonetic: "aɪ θɔt əv ə θɔt",
+    speaker: "none",
+    startTime: 285372,
+    text: "I thought of a thought,",
+    translation: "Tôi đã nghĩ về một ý nghĩ,",
+  },
+  {
+    endTime: 290763,
+    id: 31,
+    phonetic: "bət ðə θɔt aɪ θɔt ˈwəzənt ðə θɔt aɪ θɔt aɪ θɔt",
+    speaker: "none",
+    startTime: 287799,
+    text: "but the thought I thought wasn't the thought I thought I thought.",
+    translation:
+      "nhưng ý nghĩ mà tôi nghĩ không phải là ý nghĩ mà tôi nghĩ tôi đã nghĩ.",
+  },
+  {
+    endTime: 301958,
+    id: 32,
+    phonetic:
+      "ɪf ðə θɔt aɪ θɔt aɪ θɔt hæd bɪn ðə θɔt aɪ θɔt aɪ ˈwʊdənt həv θɔt soʊ mʌʧ",
+    speaker: "none",
+    startTime: 290763,
+    text: "If the thought I thought I thought had been the thought I thought, I wouldn't have thought so much.",
+    translation:
+      "Nếu ý nghĩ mà tôi nghĩ tôi đã nghĩ là ý nghĩ mà tôi đã nghĩ, tôi đã không nghĩ nhiều đến thế.",
+  },
+  {
+    endTime: 308788,
+    id: 33,
+    phonetic:
+      "ˈɛksələnt dʒəst soʊ ju noʊ ðə ˈrizən aɪm ɪnˈkludɪŋ ðɪs tʌŋ ˈtwɪstɚ",
+    speaker: "none",
+    startTime: 303542,
+    text: "Excellent, just so you know, the reason I'm including this tongue twister",
+    translation:
+      "Tuyệt vời, chỉ để bạn biết, lý do tôi bao gồm câu xoắn lưỡi này",
+  },
+  {
+    endTime: 312191,
+    id: 34,
+    phonetic: "ænd ˈɡɪvɪŋ ju ˈvɝbəl ˈprɛʃɚ tə spiːd θɪŋz ʌp",
+    speaker: "none",
+    startTime: 308788,
+    text: "and giving you verbal pressure to speed things up",
+    translation: "và tạo áp lực bằng lời nói để bạn tăng tốc",
+  },
+  {
+    endTime: 315321,
+    id: 35,
+    phonetic: "ɪz tə ɪnˈkris jʊɚ ˈɔrəl ˈmoʊtɚ skɪlz",
+    speaker: "none",
+    startTime: 312191,
+    text: "is to increase your oral motor skills.",
+    translation: "là để cải thiện kỹ năng vận động miệng của bạn.",
+  },
+  {
+    endTime: 317684,
+    id: 36,
+    phonetic: "ðæts ə spiːʧ pəˈθɑlədʒi tɝm",
+    speaker: "none",
+    startTime: 315321,
+    text: "That's a speech pathology term.",
+    translation: "Đó là một thuật ngữ trong bệnh lý ngôn ngữ.",
+  },
+  {
+    endTime: 322709,
+    id: 37,
+    phonetic:
+      "jul faɪnd ˈɡoʊɪŋ θru ðɪs ˈɛksɚˌsaɪz ˈkwɪkli wɪl bi ˈvɛri ˌbɛnəˈfɪʃəl",
+    speaker: "none",
+    startTime: 317684,
+    text: "You'll find going through this exercise quickly will be very beneficial.",
+    translation:
+      "Bạn sẽ thấy việc thực hiện bài tập này nhanh chóng sẽ rất có lợi.",
+  },
+  {
+    endTime: 328179,
+    id: 38,
+    phonetic:
+      "aɪ hoʊp jʊr ˈhævɪŋ fʌn wɪð ðiz ˈvɪdiˌoʊ ˈklæsəz ənd aɪm ˈkipɪŋ ju ˈsmaɪlɪŋ",
+    speaker: "none",
+    startTime: 322709,
+    text: "I hope you're having fun with these video classes and I'm keeping you smiling.",
+    translation:
+      "Tôi hy vọng bạn đang vui vẻ với những lớp học video này và tôi đang giữ cho bạn mỉm cười.",
+  },
+  {
+    endTime: 332873,
+    id: 39,
+    phonetic:
+      "aɪ wɑnt ju tə lɝn ənd ɪmˈpruv bət aɪ ˈɔlsoʊ wɑnt ju tə hæv ə ɡreɪt taɪm wɪð mi",
+    speaker: "none",
+    startTime: 328179,
+    text: "I want you to learn and improve, but I also want you to have a great time with me.",
+    translation:
+      "Tôi muốn bạn học hỏi và cải thiện, nhưng tôi cũng muốn bạn có khoảng thời gian tuyệt vời với tôi.",
+  },
+  {
+    endTime: 339351,
+    id: 40,
+    phonetic:
+      "ˈɔlsoʊ doʊnt fərˈɡɛt ju meɪ prɪnt aʊt ðə ʃit ðæt əˈkʌmpəniz ðɪs ˈvɪdiˌoʊ fɔr ˈpræktɪsɪŋ ˈpɝpəsəz",
+    speaker: "none",
+    startTime: 332873,
+    text: "Also, don't forget you may print out the sheet that accompanies this video for practicing purposes.",
+    translation:
+      "Ngoài ra, đừng quên bạn có thể in ra tờ giấy đi kèm với video này để luyện tập.",
+  },
+  {
+    endTime: 344318,
+    id: 41,
+    phonetic:
+      "aɪl si ju ɪn ðə nɛkst ˈvɪdiˌoʊ wɛr wi wɪl bi ˈkʌvərɪŋ waɪ saʊndz",
+    speaker: "none",
+    startTime: 339351,
+    text: "I'll see you in the next video where we will be covering Y sounds.",
+    translation:
+      "Tôi sẽ gặp bạn trong video tiếp theo, nơi chúng ta sẽ học về các âm Y.",
+  },
+  {
+    endTime: 348759,
+    id: 42,
+    phonetic: "ænd nɑt dʒəst ˈɛni waɪ saʊndz ðə ˈhɪdən waɪ saʊndz",
+    speaker: "none",
+    startTime: 344318,
+    text: "And not just any Y sounds, the hidden Y sounds.",
+    translation: "Và không chỉ là bất kỳ âm Y nào, mà là những âm Y ẩn.",
+  },
+  {
+    endTime: 353273,
+    id: 43,
+    phonetic: "ðɛr ɑr soʊ ˈmɛni ʌv ðiz ˈdɪfərənt ˈhɪdən saʊndz ɪn ˈɪŋɡlɪʃ",
+    speaker: "none",
+    startTime: 348759,
+    text: "There are so many of these different hidden sounds in English,",
+    translation: "Có rất nhiều âm ẩn khác nhau trong tiếng Anh,",
+  },
+  {
+    endTime: 355260,
+    id: 44,
+    phonetic: "ju ɑr nɑt ˈɡoʊɪŋ tə bəˈliv ɪt",
+    speaker: "none",
+    startTime: 353273,
+    text: "you are not going to believe it.",
+    translation: "bạn sẽ không tin được đâu.",
+  },
+  {
+    endTime: 361582,
+    id: 45,
+    phonetic:
+      "bət doʊnt ˈwɝi aɪ meɪk ɪt ˈrɪli ˈizi fɔr ju tə faɪnd ðɛm hɪr ðɛm ənd juz ðɛm",
+    speaker: "none",
+    startTime: 355260,
+    text: "But don't worry, I make it really easy for you to find them, hear them, and use them.",
+    translation:
+      "Nhưng đừng lo, tôi sẽ làm cho việc tìm, nghe và sử dụng chúng trở nên thật dễ dàng.",
+  },
+  {
+    endTime: 367583,
+    id: 46,
+    phonetic: "aɪm pɔl ˈɡrubɚ ənd wi ɑr naʊ wʌn stɛp ˈkloʊsɚ tə jʊr ˈmɪrəkəl",
+    speaker: "none",
+    startTime: 361582,
+    text: "I'm Paul Gruber and we are now one step closer to your miracle.",
+    translation:
+      "Tôi là Paul Gruber và chúng ta hiện đang tiến gần hơn một bước đến điều kỳ diệu của bạn.",
+  },
+  {
+    endTime: 369263,
+    id: 47,
+    phonetic: "aɪl si ju sun baɪ baɪ",
+    speaker: "none",
+    startTime: 367583,
+    text: "I'll see you soon, bye bye.",
+    translation: "Hẹn gặp lại bạn sớm, tạm biệt.",
+  },
+];
